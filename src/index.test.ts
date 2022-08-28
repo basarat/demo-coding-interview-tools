b6b98de816dbf897ceef9cdb783ece623f842bd1
Basarat Ali Syed <basaratali@gmail.com>
2022-08-28 14:00:18 +1000

    first commit

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,9 @@
-test('it should pass', () => {
+import { isPalindrome } from './index';
 
+test('should return true for Palindrome', () => {
+  expect(isPalindrome('racecar')).toBe(true);
+});
+
+test('should return false for not a Palindrome', () => {
+  expect(isPalindrome('demo')).toBe(false);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-async function main() {
-  
+/** 
+ * @return `true` if @param input is a palindrome, `false` otherwise
+ * @example `madam` => true
+ * @example `racecar` => true
+ * @example `demo` => false
+ */
+export function isPalindrome(input: string): boolean {
+  return input === input.split('').reverse().join('');
 }
-
-main();
